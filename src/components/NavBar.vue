ed358280c18f7f81199e9dc53c2a75616c99bf67
marina <marina_brunello@ajg.com>
2023-06-07 17:13:38 +1000

    Add vue router, extra files

---
+++ src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+  <MDBNavbar light bg="light" expand="lg" container>
+    <MDBNavbarBrand href="#">Navbar</MDBNavbarBrand>
+    <MDBNavbarToggler target="#navbarNav" @click="collapse2 = !collapse2"></MDBNavbarToggler>
+    <MDBCollapse id="navbarNav" v-model="collapse2">
+      <MDBNavbarNav>
+        <MDBNavbarItem to="#" active> Home </MDBNavbarItem>
+        <MDBNavbarItem to="#"> Features </MDBNavbarItem>
+        <MDBNavbarItem to="#"> Pricing </MDBNavbarItem>
+        <MDBNavbarItem to="#" disabled> Disabled </MDBNavbarItem>
+      </MDBNavbarNav>
+    </MDBCollapse>
+  </MDBNavbar>
+</template>
+
+<script>
+import { MDBNavbar, MDBNavbarBrand, MDBNavbarToggler, MDBCollapse, MDBNavbarNav, MDBNavbarItem } from 'mdb-vue-ui-kit';
+import { ref } from 'vue';
+
+export default {
+  name: 'NavBar',
+  setup() {
+    const collapse2 = ref(false);
+    return {
+      collapse2
+    }
+  },
+  components: {
+    MDBNavbar, MDBNavbarBrand, MDBNavbarToggler, MDBCollapse, MDBNavbarNav, MDBNavbarItem
+  }
+}
+
+</script>
---
+++ src/views/TaskForm.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <h1>Create a task</h1>
+    <form>
+      <label>Select a category</label>
+      <!-- <select v-model="task.category">
+        <option v-for="task in categories" :value="option" :key="option" :selected="option === task.category">{{ option
+        }}</option>
+      </select> -->
+      <h3>Name & describe your task</h3>
+      
+      <TaskInput 
+        v-model="task.title"
+        label="title"
+        type="text"
+      />
+
+      <TaskInput 
+        v-model="task.description"
+        label="Description"
+        type="text"
+      />
+
+      <h3>Where is your task?</h3>
+
+      <TaskInput 
+        v-model="task.location"
+        label="Location"
+        type="text"
+      />
+      <button type="submit">Submit</button>
+    </form>
+  </div>
+</template>
+<script>
+import TaskInput from '@/components/TaskInput.vue';
+
+export default {
+  components: {
+    TaskInput
+  },
+  data() {
+    return {
+      categories: [
+        'sustainability',
+        'nature',
+        'animal welfare',
+        'housing',
+        'education',
+        'food',
+        'community'
+      ],
+      task: {
+        category: '',
+        title: '',
+        description: '',
+        location: '',
+      }
+    }
+  }
+}
+</script>
